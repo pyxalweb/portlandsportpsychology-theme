8253ee62544324b6d64ee313ffdbff02ee8e44fc
Alex Winter <alex@pyxalweb.com>
2023-02-03 00:42:40 -0800

    Workbook section

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 get_header();
 ?>
 	<section id="site-masthead" class="masthead masthead--video" role="complementary" aria-label="Decorative looping video">
-        <div class="masthead__overlay homepage | animate-scroll-fade-in" data-delay="0">
+        <div class="masthead__overlay homepage | animate-scroll-fade-in" data-delay="0.5">
             <div class="masthead__overlay__box">
                 <div class="masthead__overlay__heading">
                     <h1>Elliott Waksman</h1>
@@ -40,7 +40,7 @@ get_header();
     </section>
 
 	<main id="site-content" class="site-content">
-        <div class="content width-df | callout-ctas even-columns even-columns--four even-columns--two--vw500 even-columns--one-vw200 even-columns--gap-5-vw200 | mbl-5 mbl-vw400-3 | animate-scroll-fade-in visible" data-delay="0.5" data-scroller="90%">
+        <section class="content width-df | callout-ctas even-columns even-columns--four even-columns--two--vw500 even-columns--one-vw200 even-columns--gap-5-vw200 | mbl-5 mbl-vw400-3 | animate-children-scroll-fade-in" data-delay="0.5" data-scroller="90%">
             <div class="callout-ctas__item">
                 <div class="callout-ctas__text">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/icon-brain.svg" alt="" width="51" height="57">
@@ -48,7 +48,7 @@ get_header();
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras commodo iaculis semper. Morbi et posuere nunc, eu venenatis arcu.</p>
                 </div>
                 <div class="callout-ctas__link">
-                    <a href="/" aria-label="Learn more about Individual Sessions">Learn More</a>
+                    <a href="/" aria-label="Learn more about Individual Sessions" class="button">Learn More</a>
                 </div>
             </div>
 
@@ -59,7 +59,7 @@ get_header();
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras commodo iaculis semper. Morbi et posuere nunc, eu venenatis arcu.</p>
                 </div>
                 <div class="callout-ctas__link">
-                    <a href="/" aria-label="Learn more about Team Workshops">Learn More</a>
+                    <a href="/" aria-label="Learn more about Team Workshops" class="button">Learn More</a>
                 </div>
             </div>
 
@@ -70,7 +70,7 @@ get_header();
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras commodo iaculis semper. Morbi et posuere nunc, eu venenatis arcu.</p>
                 </div>
                 <div class="callout-ctas__link">
-                    <a href="/" aria-label="Learn more about Parenting Workshops">Learn More</a>
+                    <a href="/" aria-label="Learn more about Parenting Workshops" class="button">Learn More</a>
                 </div>
             </div>
 
@@ -81,30 +81,72 @@ get_header();
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras commodo iaculis semper. Morbi et posuere nunc, eu venenatis arcu.</p>
                 </div>
                 <div class="callout-ctas__link">
-                    <a href="/" aria-label="Learn more about Student Mentoring">Learn More</a>
+                    <a href="/" aria-label="Learn more about Student Mentoring" class="button">Learn More</a>
                 </div>
             </div>
-        </div>
+        </section>
 
-        <div class="content-wide | mbl-5 mbl-vw400-3 bg-grey-100">
+        <section class="content-wide | mbl-5 mbl-vw400-3 bg-grey-100 bg-grey-swoop-01 | animate-scroll-fade-in" data-delay="0.5" data-scroller="90%">
             <div class="content width-df | split-content even-columns even-columns--two even-columns--one-vw200 even-columns--gap-0-vw200">
-                <div class="split-content__text | pbl-5 pbl-vw400-3">
+                <div class="split-content__text | pbl-5 pbl-vw400-3 | animate-scroll-slide-horizontal" data-delay="0.5" data-scroller="90%" data-duration="0.75" data-position="-300">
                     <div>
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/icon-light-bulb.svg" alt="" width="39" height="57">
                         <h2>Elliott Waksman</h2>
-                        <div class="sub-text">
+                        <div class="gap-line | text-7">
                             <p>Waksman helps student-athletes play better and find more enjoyment in their sport.</p>
                         </div>
                         <p>Sport psychology techniques are life skills that every student-athlete can apply to improve their game and quality of life. Confidence, commitment, composure, courage, teamwork, and goal setting are arguably the most important aspects of youth sports and are topics Waksman often addresses in his sessions.</p>
-                        <a href="/" aria-label="Learn more about Elliott Waksman" class="button">Learn More</a>
+                        <a href="/" aria-label="Learn more about Elliott Waksman" class="button">About Elliott</a>
                     </div>
                 </div>
 
-                <div class="split-content__image | pb-3-vw200">
+                <div class="split-content__image split-content__image--circle--mobile | pb-3-vw200 | animate-scroll-slide-horizontal" data-delay="0.5" data-scroller="90%" data-duration="0.75" data-position="300">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/elliott-waksman.jpg" alt="Elliott Waksman - Sport Psychologist in Portland, Oregon">
                 </div>
             </div>
-        </div>
+        </section>
+
+        <section class="content-wide | mbl-5 mbl-vw400-3 bg-grey-100 bg-grey-swoop-02 | animate-scroll-fade-in" data-delay="0.5" data-scroller="90%">
+            <div class="content width-df | split-content even-columns even-columns--two even-columns--two--reverse even-columns--one-vw200 even-columns--gap-0-vw200">
+                <div class="split-content__text | pbl-5 pbl-vw400-3 | animate-scroll-slide-horizontal" data-delay="0.5" data-scroller="90%" data-duration="0.75" data-position="300">
+                    <div>
+                        <h2>Waksman's Workbook</h2>
+                        
+                        <div class="points | mt-1">
+                            <div class="points__item">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icon-light-bulb.svg" alt="" width="39" height="57">
+                                <div class="points__text">
+                                    <h3>Improve Self-Confidence</h3>
+                                    <p>Aenean lacinia bibendum nulla sed consectetur. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Fusce.</p>
+                                </div>
+                            </div>
+
+                            <div class="points__item">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icon-light-bulb.svg" alt="" width="39" height="57">
+                                <div class="points__text">
+                                    <h3>Increase Motivation Levels</h3>
+                                    <p>Aenean lacinia bibendum nulla sed consectetur. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Fusce.</p>
+                                </div>
+                            </div>
+
+                            <div class="points__item">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icon-light-bulb.svg" alt="" width="39" height="57">
+                                <div class="points__text">
+                                    <h3>Success On & Off the Field</h3>
+                                    <p>Aenean lacinia bibendum nulla sed consectetur. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Fusce.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <a href="/" aria-label="Order Waksman's Workbook Now" class="button">Order Now</a>
+                    </div>
+                </div>
+
+                <div class="split-content__image split-content__image--scale-down | pb-3-vw200 | animate-scroll-slide-horizontal" data-delay="0.5" data-scroller="90%" data-duration="0.75" data-position="-300">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/waksmans-sport-psychology-workbook.png" alt="Waksman's Sport Psychology Program: A Practical Mental Skills Workbook for Student Athletes">
+                </div>
+            </div>
+        </section>
 
         <!--
         <div class="content">
