848512f532cc8e997f0dc9c93eebc9bd28aa396a
Alex Winter <alex@pyxalweb.com>
2023-02-25 21:49:01 -0800

    Progress made on Workbook page

--- page-waksmans-workbook.php
+++ page-waksmans-workbook.php
@@ -15,10 +15,11 @@ get_template_part( 'template-parts/content', 'masthead' );
 <main id="site-content" <?php post_class($class = 'site-content interior'); ?>>
 	<?php //get_template_part( 'template-parts/content', 'page' ); ?>
 
-    <section class="content width-df | mbl-7 mbl-3-vw400 | animate-children-scroll-fade-in" data-delay="0.5" data-scroller="90%">
-        <h2>Lorem ipsum dolor sit em heading</h2>
+    <section class="content width-df | mbl-7 mbl-3-vw400 ta-c | animate-children-scroll-fade-in" data-delay="0.5" data-scroller="90%">
+        <h2>Waksman's Sport Psychology Program</h2>
+        <p><strong>A Practical Mental Skills Workbook for Student-Athletes</strong></p>
         <p>This workbook is a resource for student-athletes looking to improve sport performance and learn valuable life skills.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat impedit earum et ipsa reprehenderit vero sint, quod quisquam, labore voluptate pariatur laboriosam ipsum autem reiciendis officiis quia. Veniam, consectetur maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi illum repudiandae quaerat. Ut facere totam quasi dolores laborum tempora rerum reprehenderit vero nesciunt asperiores voluptatibus magni consequuntur cupiditate, adipisci incidunt.</p>
+        <p>Elliott Waksman's Sport Psychology Program teaches mental skills, which when consistently incorporated, lead to success on and off the field. Upon completion of the workbook, the student-athlete will be able to proficiently complete the following tasks: improve self-confidence, maintain a more mentally tough approach throughout training and competition, and describe how life skills are learned through sport.</p>
     </section>
 
     <section class="content-wide | bg-grey-swoop-02 | animate-scroll-fade-in" data-delay="0.5" data-scroller="90%">
@@ -52,8 +53,6 @@ get_template_part( 'template-parts/content', 'masthead' );
                             </div>
                         </div>
                     </div>
-
-                    <a href="/" aria-label="Order Waksman's Workbook Now" class="button">Order Now</a>
                 </div>
             </div>
 
@@ -61,37 +60,16 @@ get_template_part( 'template-parts/content', 'masthead' );
                 <img src="<?php echo get_template_directory_uri(); ?>/photos/waksmans-sport-psychology-workbook.png" alt="Waksman's Sport Psychology Program: A Practical Mental Skills Workbook for Student Athletes">
             </div>
         </div>
-
-        <div class="text-wrap | text-2 ta-c | pb-5 pb-4-vw400">
-            <p>$350 per workshop<br>Additional travel fees may apply</p>
-        </div>
     </section>
 
-
-
-
-
-
-
-
-
-    <section class="content width-df | split-content even-columns even-columns--two even-columns--one-vw200 | mbl-4 | animate-children-scroll-fade-in" data-delay="0.5" data-scroller="90%">
-        <div class="split-content__text | mb-1--h2">
-            <div>
-                <?php echo get_field('split_content_text_1'); ?>
-                <a href="#" class="button | mt-1">Schedule Consultation</a>
-            </div>
+    <section class="content width-xs | mbl-7 mbl-3-vw400 ta-c | animate-children-scroll-fade-in" data-delay="0.5" data-scroller="90%">
+        <h2>Order Now!</h2>
+        <div class="text-3">
+            <p>$30</p>
         </div>
-
-        <div class="split-content__image split-content__image--circle--mobile split-content__image--rounded">
-            <?php
-            $image = get_field('split_content_image_1');
-            if ($image):
-                $url = $image['url'];
-                $alt = $image['alt'];
-            ?>
-            <img src="<?php echo $url ?>" alt="<?php echo $alt ?>" width="1920" height="500">
-            <?php endif; ?>
+        <p>(Free shipping!)</p>
+        <div class="mt-1">
+            <script type="text/javascript" src="https://form.jotform.com/jsform/230560584312147"></script>
         </div>
     </section>
 </main>
