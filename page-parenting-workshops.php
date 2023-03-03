eb37381df0996c4546eebfe8dbb20661a5d137dd
Alex Winter <alex@pyxalweb.com>
2023-03-02 23:49:52 -0800

    Updated 'Parenting Workshops' and 'Student Mentoring' to match other two Services pages' edits

--- page-parenting-workshops.php
+++ page-parenting-workshops.php
@@ -15,15 +15,32 @@ get_template_part( 'template-parts/content', 'masthead' );
 <main id="site-content" <?php post_class($class = 'site-content interior'); ?>>
 	<?php //get_template_part( 'template-parts/content', 'page' ); ?>
 
-    <section class="content width-df | split-content even-columns even-columns--two even-columns--one-vw200 | mbl-4 | animate-children-scroll-fade-in" data-delay="0.5" data-scroller="90%">
+    <section class="content width-df | split-content even-columns even-columns--two even-columns--prioritize-text--vw800 even-columns--one-vw500 | mbl-4 | animate-children-scroll-fade-in" data-delay="0.5" data-scroller="90%">
         <div class="split-content__text | mb-1--h2">
             <div>
                 <?php echo get_field('split_content_text_1'); ?>
-                <a href="/contact" class="button | mt-1">Schedule Consultation</a>
+                
+                <div class="contain | mt-2">
+                    <div class="list">
+                        <?php echo get_field('list_items'); ?>
+                    </div>
+
+                    <div class="sale">
+                        <div class="text-2">
+                            <p><?php echo get_field('cost'); ?></p>
+                        </div>
+
+                        <a href="/contact" class="button button--highlight">Schedule Consultation</a>
+
+                        <div class="text-10">
+                            <p><?php echo get_field('travel_fees'); ?></p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
-        <div class="split-content__image split-content__image--circle--mobile split-content__image--rounded | pb-3-vw200">
+        <div class="split-content__image split-content__image--rounded split-content__image--circle--mobile | pb-3-vw200">
             <?php
             $image = get_field('split_content_image_1');
             if ($image):
