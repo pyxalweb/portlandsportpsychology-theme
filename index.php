76ec2d9ff4e67f727c63014b3dadc4689284683c
Alex Winter <alex@pyxalweb.com>
2023-03-07 23:14:33 -0800

    Fixed workbook text centering and padding-top issue on small viewports

--- index.php
+++ index.php
@@ -160,9 +160,9 @@ get_header();
     </section>
 
     <section class="content-wide | mt-7 mt-3-vw400 bg-grey-swoop | animate-scroll-fade-in" data-delay="0.5" data-scroller="90%">
-        <div class="content width-df | dg gtc-2 g-clamp-1-3-3 | gtc-1-vw200">
+        <div class="content width-df | dg gtc-2 g-clamp-1-3-3 | gtc-1-vw200 pt-3-vw200">
             <div class="df fd-c jc-c | o-2 | o-1-vw500 | pbl-5 pbl-3-vw400 | animate-scroll-slide-horizontal" data-delay="0.5" data-scroller="90%" data-duration="0.75" data-position="300">
-                <div class="df fd-c g-1 | text-6--h2">
+                <div class="df fd-c g-1 | ta-c-vw200 | text-6--h2">
                     <h2>Waksman's Workbook</h2>
                     
                     <div class="points | text-4--h3 | mt-1">
