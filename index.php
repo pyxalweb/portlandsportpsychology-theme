5242011dd46707c82c9427254245b6a06188fdd3
Alex Winter <alex@pyxalweb.com>
2023-04-04 23:54:47 -0700

    Applied 'content-photo' classes to Workbook sections

--- index.php
+++ index.php
@@ -160,14 +160,14 @@ get_header();
     </section>
 
     <section class="mt-7 mt-3-vw400 bg-grey-swoop | animate-scroll-fade-in" data-delay="0.5" data-scroller="90%" style="opacity:0;">
-        <div class="content width-df | dg gtc-2 g-clamp-1-3-3 | gtc-1-vw200 pt-3-vw200">
-            <div class="df fd-c jc-c | o-2 | o-1-vw500 | pbl-5 pbl-3-vw400 pt-0-vw200 | animate-scroll-slide-horizontal" data-delay="0.5" data-scroller="90%" data-duration="0.75" data-position="300">
-                <div class="df fd-c g-1 | ta-c-vw200 | text-6--h2">
+        <div class="content-photo | content-photo--gtc-1-vw300 | content width-df | pt-3-vw300">
+            <div class="content-photo__container | o-2 | o-1-vw500 | pbl-5 pbl-3-vw300 pt-0-vw300 | animate-scroll-slide-horizontal" data-delay="0.5" data-scroller="90%" data-duration="0.75" data-position="300">
+                <div class="content-photo__text | ta-c-vw300 | text-6--h2">
                     <h2>Waksman's Workbook</h2>
                     
                     <div class="points | text-4--h3 | mt-1">
                         <div class="points__item">
-                            <div class="svg svg--57h | mi-center-vw200">
+                            <div class="svg svg--57h | mi-center-vw300">
                                 <?php load_inline_svg('icon-smile.svg'); ?>
                             </div>
                             <div class="points__text">
@@ -177,7 +177,7 @@ get_header();
                         </div>
 
                         <div class="points__item">
-                            <div class="svg svg--57h | mi-center-vw200">
+                            <div class="svg svg--57h | mi-center-vw300">
                                 <?php load_inline_svg('icon-trophy.svg'); ?>
                             </div>
                             <div class="points__text">
@@ -187,7 +187,7 @@ get_header();
                         </div>
 
                         <div class="points__item">
-                            <div class="svg svg--57h | mi-center-vw200">
+                            <div class="svg svg--57h | mi-center-vw300">
                                 <?php load_inline_svg('icon-graph.svg'); ?>
                             </div>
                             <div class="points__text">
@@ -197,13 +197,13 @@ get_header();
                         </div>
                     </div>
 
-                    <a href="/waksmans-workbook" aria-label="Order Waksman's Workbook Now" class="button button--highlight | mt-2-vw200 mi-center-vw200">Order Now</a>
+                    <a href="/waksmans-workbook" aria-label="Order Waksman's Workbook Now" class="button button--highlight | mt-2-vw300 mi-center-vw300">Order Now</a>
                 </div>
             </div>
 
-            <div class="df fd-c | image--maxheight-760 image--objectfit--scaledown | animate-scroll-slide-horizontal" data-delay="0.5" data-scroller="90%" data-duration="0.75" data-position="-300">
+            <div class="content-photo__container | image--maxheight-760 | image--maxheight-reset | image--objectfit--scaledown | animate-scroll-slide-horizontal | mi-center-child | pt-4-vw300" data-delay="0.5" data-scroller="90%" data-duration="0.75" data-position="-300">
                 <picture>
-                    <source srcset="<?php echo get_template_directory_uri(); ?>/photos/waksmans-sport-psychology-workbook-no-shadow.png" type="image/png" media="(max-width: 640px)">
+                    <source srcset="<?php echo get_template_directory_uri(); ?>/photos/waksmans-sport-psychology-workbook-no-shadow.png" type="image/png" media="(max-width: 800px)">
                     <img src="<?php echo get_template_directory_uri(); ?>/photos/waksmans-sport-psychology-workbook.png" alt="Waksman's Sport Psychology Program: A Practical Mental Skills Workbook for Student Athletes">
                 </picture>
             </div>
--- page-waksmans-workbook.php
+++ page-waksmans-workbook.php
@@ -39,15 +39,15 @@ endwhile;
         <a href="#order-now" class="button button--highlight | mi-center mt-2">Order Now</a>
     </section>
 
-    <section class="content-wide | bg-grey-swoop | animate-scroll-fade-in" data-delay="0.5" data-scroller="90%" style="opacity:0;">
-        <div class="content width-df | dg gtc-2 g-clamp-1-3-3 | gtc-1-vw200 pt-3-vw200">
-            <div class="df fd-c jc-c | o-2 | o-1-vw500 | pbl-5 pbl-3-vw400 pt-0-vw200 | animate-scroll-slide-horizontal" data-delay="0.5" data-scroller="90%" data-duration="0.75" data-position="300">
-                <div class="df fd-c g-1 | ta-c-vw200 | text-6--h2">
+    <section class="mt-7 mt-3-vw400 bg-grey-swoop | animate-scroll-fade-in" data-delay="0.5" data-scroller="90%" style="opacity:0;">
+        <div class="content-photo | content-photo--gtc-1-vw300 | content width-df | pt-3-vw300">
+            <div class="content-photo__container | o-2 | o-1-vw500 | pbl-5 pbl-3-vw400 pt-0-vw300 | animate-scroll-slide-horizontal" data-delay="0.5" data-scroller="90%" data-duration="0.75" data-position="300">
+                <div class="content-photo__text | ta-c-vw300 | text-6--h2">
                     <h2>Beyond Physical Training</h2>
                     
                     <div class="points | text-4--h3 | mt-1">
                         <div class="points__item">
-                            <div class="svg svg--57h | mi-center-vw200">
+                            <div class="svg svg--57h | mi-center-vw300">
                                 <?php load_inline_svg('icon-smile.svg'); ?>
                             </div>
                             <div class="points__text">
@@ -57,7 +57,7 @@ endwhile;
                         </div>
 
                         <div class="points__item">
-                            <div class="svg svg--57h | mi-center-vw200">
+                            <div class="svg svg--57h | mi-center-vw300">
                                 <?php load_inline_svg('icon-trophy.svg'); ?>
                             </div>
                             <div class="points__text">
@@ -67,7 +67,7 @@ endwhile;
                         </div>
 
                         <div class="points__item">
-                            <div class="svg svg--57h | mi-center-vw200">
+                            <div class="svg svg--57h | mi-center-vw300">
                                 <?php load_inline_svg('icon-graph.svg'); ?>
                             </div>
                             <div class="points__text">
@@ -79,9 +79,9 @@ endwhile;
                 </div>
             </div>
 
-            <div class="df fd-c | image--maxheight-760 image--objectfit--scaledown | animate-scroll-slide-horizontal" data-delay="0.5" data-scroller="90%" data-duration="0.75" data-position="-300">
+            <div class="content-photo__container | image--maxheight-760 | image--maxheight-reset | image--objectfit--scaledown | animate-scroll-slide-horizontal | mi-center-child | pbl-4 pb-0-vw300" data-delay="0.5" data-scroller="90%" data-duration="0.75" data-position="-300">
                 <picture>
-                    <source srcset="<?php echo get_template_directory_uri(); ?>/photos/waksmans-sport-psychology-workbook-no-shadow.png" type="image/png" media="(max-width: 640px)">
+                    <source srcset="<?php echo get_template_directory_uri(); ?>/photos/waksmans-sport-psychology-workbook-no-shadow.png" type="image/png" media="(max-width: 800px)">
                     <img src="<?php echo get_template_directory_uri(); ?>/photos/waksmans-sport-psychology-workbook.png" alt="Waksman's Sport Psychology Program: A Practical Mental Skills Workbook for Student Athletes" width="676" height="760">
                 </picture>
             </div>
