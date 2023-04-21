083b5d1fdefe4dd5082f945817fd4ce1adfdbf7e
Alex Winter <alex@pyxalweb.com>
2023-04-20 20:09:25 -0700

    Removed unnecessary markup that was commented out

--- header.php
+++ header.php
@@ -51,39 +51,3 @@
     <a href="/contact">Schedule Consultation</a>
 	<a href="/waksmans-workbook">Waksman's Workbook</a>
 </div>
-
-
-	<?php /*
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$psp_description = get_bloginfo( 'description', 'display' );
-			if ( $psp_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $psp_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'psp' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-	*/ ?>
