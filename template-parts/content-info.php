c18635e108cfcf265ca28e89d17375259cf42d19
Alex Winter <alex@pyxalweb.com>
2023-04-20 21:52:12 -0700

    Added comment to remind me how to edit this later

--- template-parts/content-info.php
+++ template-parts/content-info.php
@@ -1,3 +1,7 @@
+<?php /*
+The following data is pulled from the 'AW Contact Info' plugin
+You can edit this via the WP Admin Dashboard in 'Settings' > 'Contact Info'
+*/ ?>
 <h2><?php echo contact_info_title(); ?></h2>
 <p><?php echo contact_info_tagline(); ?></p>
 <div class="icon icon--address">
